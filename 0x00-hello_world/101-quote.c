bc287387f4bca17a99e0289ef7abd1eb92cee6ea
Udoette Mishael <mudoette01@yahoo.com>
2022-09-12 06:53:15 -0700

    commit 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-include <stdio.h>
+#include<stdio.h>
 
 /**
  * main - Entry point
@@ -8,5 +8,6 @@ include <stdio.h>
 int main(void)
 {
 	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
 	return (1);
 }
